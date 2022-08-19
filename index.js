e8ff2ab024267de4bb4c36fcccaf31bbcbe72cdf
Noela-Murugi <murugin254@gmail.com>
2022-08-19 12:08:56 +0300

    filtering arrays practice

---
+++ index.js
@@ -0,0 +1,48 @@
+//filtering-arrays practice
+
+const users = [
+    {
+      firstName: "Niky",
+      lastName: "Morgan",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Jaguar",
+    },
+    {
+      firstName: "Tracy",
+      lastName: "Lum",
+      favoriteColor: "Yellow",
+      favoriteAnimal: "Penguin",
+    },
+    {
+      firstName: "Josh",
+      lastName: "Rowley",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Penguin",
+    },
+    {
+      firstName: "Kate",
+      lastName: "Travers",
+      favoriteColor: "Red",
+      favoriteAnimal: "Jaguar",
+    },
+    {
+      firstName: "Avidor",
+      lastName: "Turkewitz",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Penguin",
+    },
+    {
+      firstName: "Drew",
+      lastName: "Price",
+      favoriteColor: "Yellow",
+      favoriteAnimal: "Elephant",
+    },
+  ];
+
+  function firstNamePrinter(collection) {
+    for (const user of collection) {
+      console.log(user.firstName);
+    }
+  }
+
+  firstNamePrinter(users);
